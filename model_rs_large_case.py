9b3a5025a7d8a018b40dab6d87e488988b50e511
lovhang <77312068+lovhang@users.noreply.github.com>
2024-01-01 03:53:44 -0500

    update

--- model_rs_large_case.py
+++ model_rs_large_case.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from docplex.mp.model import Model
 from docplex.mp import conflict_refiner
 from docplex.mp.relaxer import Relaxer
@@ -7,6 +8,8 @@ import random as random
 import pickle
 import time
 from tqdm import tqdm
+
+
 
 # read network from pkl and transfer it to t_ij^m
 num=0 #initiate num
@@ -20,6 +23,7 @@ Xiaohang = 0
 with open('case/casenum1011_x_y_ttn_spn_type_dtime_count.pkl', 'rb') as f:
      x_cord, y_cord, tt, sp, type, depart_time, passenger_count = pickle.load(f)
      f.close()
+#print(sp[1][187][1])
 #=========add dummy node 0 and n+1=============
 x_min = x_cord[0]
 x_max = x_cord[-1]
@@ -50,9 +54,9 @@ for i in range(len(type)-1): #last one is super driver
     else:
         print("exceptional type!")
         break
-N_1 = np.array(n_1)
-N_2 = np.array(n_2)
-N_3 = np.array(n_3)
+N_1 = np.array(n_1) # driver
+N_2 = np.array(n_2) # flexible driver
+N_3 = np.array(n_3) # passenger
 N_D = np.array([0 for i in range(0,num)])
 N_dummy = np.array([0, num - 1])
 print(len(N))
@@ -88,19 +92,19 @@ N_23 = np.sort(N_23)
 dm = np.array([0.0 for i in N])
 #dm_seed = random.choices(range(1,3),k=num)
 for i in range(len(passenger_count)):
-    dm[i+1] = passenger_count[i]
+    dm[i+1] = max(1,passenger_count[i])
 
 cv = np.array([0.0 for i in N])
-for i in range(0,num):
-    cv[i] = random.randint(3,6)
+for i in range(0, num):
+    cv[i] = max(dm[i], random.randint(3,5))
 #cv_seed = random.choices(range(3,5),k=num)
 T_end = 1000
 #sec_T = [500,1000]
 #cenTimes.append(T_end)
 
 # Tolerance
-LDP = np.array([T_end for i in range(0, num)])  # latest departure time, the time location is affected
-EDP = np.array([0 for i in range(0, num)])  # Earliest departure time
+LDP = np.array([T_end for i in N])  # latest departure time, the time location is affected
+EDP = np.array([0 for i in N])  # Earliest departure time
 time_window = np.random.uniform(30,60,num) # time window uniformly distributed
 for i in range(len(depart_time)):
     EDP[i+1] = depart_time[i]
@@ -110,20 +114,35 @@ for i in range(len(depart_time)):
   #  EDP[i] = temp
    # LDP[i] = temp + time_window[i]
 
-DT = np.array([0 for i in range(0, num)])  # Detour time
+DT = np.array([0 for i in N])  # Detour time
 #dtrate = 1.5 #detour rate
-heuristic_ratio = 1.0 # should less than detour ratio
-for i in N_e:
+heuristic_ratio = [[0.0,0.0,0.0,0.0,0.0,0.0],[8.0,6.0,2.0,1.0,1.0,1.0]]# should less than detour ratio
+for i in N_1:
     for k in sc_set:
         if EDP[i] + tt[i][N_D[i]][k] <= ScenTimes[k]:
             DT[i] = round(tt[i][N_D[i]][k] * random.uniform(1.1, 1.5)) #detour rate
-min_flex_interval = 20 # minmum flexible time interval for per passenger picked up.
+for i in N_2:
+    for k in sc_set:
+        if EDP[i] + tt[i][N_D[i]][k] <= ScenTimes[k]:
+            DT[i] = round(tt[i][N_D[i]][k] * random.uniform(1.1, 1.5)) #detour rate
+for i in N_3:
+    for k in sc_set:
+        if EDP[i] + tt[i][N_D[i]][k] <= ScenTimes[k]:
+            DT[i] = round(tt[i][N_D[i]][k] * random.uniform(1.1, 1.5))  # detour rate
+min_flex_interval = 5 # minmum flexible time interval for per passenger picked up.
 #for i in N_1:
     #print(EDP[i], LDP[i], DT[i], tt[i][N_D[i]])
-Largest_state_num = 1000000
-
-
+Largest_state_num = 500000
 tts = np.divide(tt, 100)  # Super driver travel time
+
+
+#with open('case/param_1_1011_dm_cv_EDP_LDP_DT.pkl', 'rb') as f:
+#    dm, cv, EDP, LDP, DT = pickle.load(f)
+#    f.close()
+
+#e_param = pd.DataFrame(
+#    {'id': N, 'demand': dm, 'capacity': cv, 'earlist_departure': EDP, 'lateste_departure': LDP, 'detour': DT})
+#e_param.to_csv('case/case1_param_1011.csv')
 
 M = 20000
 Mhalf = 10000
@@ -136,7 +155,21 @@ x_var = np.zeros((num, num, num, scenario))  # x(i, j, v, m)
 y_var = np.zeros((num, num))  # q(i, v)
 r_var = np.zeros((num, num))  # r(i, v)
 
+def save_current_network_para():
+    with open('case/param_1_{n}_dm_cv_EDP_LDP_DT.pkl'.format(n=num), 'wb') as f:
+        pickle.dump([dm,cv, EDP, LDP, DT], f)
+    f.close()
+#def load_current_network_para(dm,cv,EDP,LDP,DT):
+#    e_param = pd.DataFrame(
+#        {'id': N, 'demand': dm, 'capacity': cv, 'earlist_departure': EDP, 'lateste_departure': LDP, 'detour': DT})
+#    with open('case/param1011_dm_cv_EDP_LDP_DT.pkl', 'rb') as f:
+#        dm, cv, EDP, LDP, DT = pickle.load(f)
+#        f.close()
 
+#    e_param = pd.DataFrame(
+#        {'id': N, 'demand': dm, 'capacity': cv, 'earlist_departure': EDP, 'lateste_departure': LDP, 'detour': DT})
+#    e_param.head(10)
+#    e_param.to_csv('case/case1_param_1011.csv')
 class model:
     def __init__(self):
         self.mdl = Model(name='Evacuation')
@@ -321,8 +354,9 @@ class state:
 
 
 class dynapro:
-    def __init__(self):
+    def __init__(self, ratio):
         print("initiate")
+        self.ratio = ratio # different heuristic ratio
         self.solutionpoll = []
         self.Ncan = []
         self.candidateState = []
@@ -347,7 +381,7 @@ class dynapro:
             s1 = state(S, R, T, dt, Q, route, last, alternative, e_num)
             self.solutionpoll.append(s1)
         self.state_num = len(self.solutionpoll)
-    def action(self, inputstate: state, des: int):
+    def action(self, inputstate: state, des: int, it_num: int):
         S = inputstate.S.copy()
         R = inputstate.R.copy()
         T = inputstate.T.copy()
@@ -380,7 +414,7 @@ class dynapro:
                             dt = []
                             Q_j = 0
                             S.append(des)
-                            #S.append(N[-1])
+                            S.append(N[-1])
                             T.append(T_j)
                             Q.append(Q_j)
                             route.append(sp[l][des][i])
@@ -396,22 +430,60 @@ class dynapro:
                     T_j = T[-1] + tt[l][des][i]  #
                     Q_j = Q[-1]
                     dtmin = min(dt)
-                    latest_ar_time = min(ScenTimes[i], dtmin)
-                    if T_j + min_flex_interval <= latest_ar_time: #deleted DT[des] here, not sure
-                        for j in range(len(R) - 1, -1, -1):
-                            if N_D[R[j]] == des:
-                                Q_j -= dm[R[j]]
-                                del R[j]
-                                del dt[j]
+                    feasi = True  # check if there node that is visited before is not able arrivle its destination by visiting node 'des'
+                    # addtime = 0.0
+                    for j in range(0, len(R)):
+                        tempf = False
+                        for k in range(len(tt[des][N_D[j]])):
+                            tempt = T_j + tt[des][N_D[j]][k]
+                            if tempt + min_flex_interval < dt[j] and tempt + min_flex_interval <= ScenTimes[
+                                k] and tempt + min_flex_interval <= dt[0]:
+                                tempf = True
 
-                        S.append(des)
-                        T.append(T_j)
-                        Q.append(Q_j)
-                        route.append(sp[l][des][i])
-                        l = des
-                        alternative.append(temp_alt)
-                        #flex_time = min(flex_time, latest_ar_time - T_j)
-                        return state(S, R, T, dt, Q, route, l,alternative, e_num)
+                                break
+                        if tempf == False:
+                            feasi = False
+                            break
+                    # =======================end of elimination rule 1======================================================
+                    if feasi == False:
+                        return None
+                    else:
+                        # ========================elimination rule 5 =====================================
+                        des_set = [N_D[des]]
+                        for item in R:
+                            if N_D[item] not in des_set:
+                                des_set.append(N_D[item])
+                        des_TT = 0
+                        if len(des_set) == 1:
+                            for k in sc_set:
+                                des_TT += tt[des][des_set[0]][k]
+                            des_TT = des_TT / scenario
+                        else:
+                            des_TT = 0
+                            for l in sc_set:
+                                des_TT = tt[des_set[-1]][des_set[0]][l]
+                                for k in range(len(des_set) - 1):
+                                    des_TT = tt[des_set[k]][des_set[k + 1]][l]
+                            des_TT = des_TT / scenario
+                        if T_j + des_TT * self.ratio[it_num] + min_flex_interval > dt[0]:
+                            return None
+                        else:
+                            latest_ar_time = min(ScenTimes[i], dtmin)
+                            if T_j + min_flex_interval <= latest_ar_time: #deleted DT[des] here, not sure
+                                for j in range(len(R) - 1, -1, -1):
+                                    if N_D[R[j]] == des:
+                                        Q_j -= dm[R[j]]
+                                        del R[j]
+                                        del dt[j]
+
+                                S.append(des)
+                                T.append(T_j)
+                                Q.append(Q_j)
+                                route.append(sp[l][des][i])
+                                l = des
+                                alternative.append(temp_alt)
+                                #flex_time = min(flex_time, latest_ar_time - T_j)
+                                return state(S, R, T, dt, Q, route, l,alternative, e_num)
                     temp_alt += 1
                 return None
         elif des in N_e:  # if des is node of evacuees
@@ -460,22 +532,23 @@ class dynapro:
                                 for k in range(len(des_set)-1):
                                     des_TT = tt[des_set[k]][des_set[k+1]][l]
                             des_TT = des_TT/scenario
-                        if T_j + des_TT*heuristic_ratio+min_flex_interval> dt[0]:
+                        if T_j + des_TT*self.ratio[it_num]+min_flex_interval> dt[0]:
                             return None
+                        else:
 #=======================end of elimination rule 5======================================================
-                        latest_ar_time = min(ScenTimes[i], LDP[des], dtmin)
-                        if T_j + min_flex_interval <= latest_ar_time and Q_j <= cv[S[1]]:
-                            R.append(des)
-                            S.append(des)
-                            T.append(T_j)
-                            dt.append(T_j + DT[des])
-                            Q.append(Q_j)
-                            route.append(sp[l][des][i])
-                            l = des
-                            alternative.append(temp_alt)
-                            e_num += 1
-                            #flex_time = min(flex_time, latest_ar_time)
-                            return state(S, R, T, dt, Q, route, l, alternative, e_num)
+                            latest_ar_time = min(ScenTimes[i], LDP[des], dtmin)
+                            if T_j + min_flex_interval <= latest_ar_time and Q_j <= cv[S[1]]:
+                                R.append(des)
+                                S.append(des)
+                                T.append(T_j)
+                                dt.append(T_j + DT[des])
+                                Q.append(Q_j)
+                                route.append(sp[l][des][i])
+                                l = des
+                                alternative.append(temp_alt)
+                                e_num += 1
+                                #flex_time = min(flex_time, latest_ar_time)
+                                return state(S, R, T, dt, Q, route, l, alternative, e_num)
                     temp_alt += 1
                 return None
         else:
@@ -486,7 +559,7 @@ class dynapro:
             tempslopol = input
             returnstate = []
             if len(tempslopol) >= 1:
-                self.ite += 1
+                #self.ite += 1
                 print('====iteration: {i}===='.format(i=self.ite))
                 print('====current states num: {num} ,start to extend state===='.format(num=len(self.solutionpoll)))
                 for item in tqdm(tempslopol):
@@ -515,7 +588,7 @@ class dynapro:
                     #print(item.S)
                     for des in can_node:
                             # print('des', des)
-                        tpstate = self.action(item, des)
+                        tpstate = self.action(item, des, self.ite)
                         if tpstate != None:
                             if tpstate.last == N[-1]:
                                 self.candidateState.append(tpstate)
@@ -537,6 +610,7 @@ class dynapro:
                     raise Exception("large state number")
                 self.state_num = self.state_num + state_num
                 return self.rundp(self.solutionpoll)
+                self.ite += 1
             else:
                 print("======end iteration, candidate route:======")
 
@@ -570,7 +644,11 @@ class dynapro:
             if (de == False):
                 uniqueState.append(state1)
         return uniqueState
-
+    def output_trip_attribute(self):
+        trip_length=[0 for i in range(0,20)]
+        for item in self.candidateState:
+            trip_length[item.e_num] +=1
+        return trip_length
 class dymaster:
     def __init__(self, input: list):
         self.a = [[] for i in range(num)]  # v,k,i
@@ -578,15 +656,16 @@ class dymaster:
         self.route2 = [[] for i in range(num)] # all underlying routes for alternative routs for v
         self.route_num = [0 for i in range(num)]
         self.alternate = [[] for _ in range(num)]
-        for item in input:
-            temp = [0.0 for i in range(num)]
-            self.route[item.S[1]].append(item.S)
-            self.route2[item.S[1]].append(item.route)
-            self.alternate[item.S[1]].append(item.alternative)
-            for _ in N_e:
-                if _ in item.S:
-                    temp[_] = 1.0
-            self.a[item.S[1]].append(temp)
+        for i in range(0,len(input)):
+            for item in input[i]:
+                temp = [0.0 for i in range(num)]
+                self.route[item.S[1]].append(item.S)
+                self.route2[item.S[1]].append(item.route)
+                self.alternate[item.S[1]].append(item.alternative)
+                for _ in N_e:
+                    if _ in item.S:
+                        temp[_] = 1.0
+                self.a[item.S[1]].append(temp)
         for i in range(num):
             self.route_num[i] = len(self.route[i])
         #for _ in Vh:
@@ -776,26 +855,29 @@ def main(triger: int):
     # ============run model on dynamic programming==========
     elif triger == 1:
         print("start dynamic programming")
-        ts = dynapro()
         start = time.time()
-        ts.rundp(ts.solutionpoll)
-        print("generate {state_num} states totally".format(state_num = ts.state_num))
+        candidate_state = []
+        for i in range(0, len(heuristic_ratio)):
+            ts = dynapro(heuristic_ratio[i])
+            ts.rundp(ts.solutionpoll)
+            candidate_state.append(ts.candidateState)
+            print('========solution_arrtibutes_for_iteration '+str(i)+' ========')
+            print(ts.output_trip_attribute())
+            print("generate {state_num} states".format(state_num = ts.state_num))
         end = time.time()
-        ite = 0
-        for item in ts.candidateState:
-            #print(ite)
-            ite += 1
-        dyma = dymaster(ts.candidateState)
+        dyma = dymaster(candidate_state)
         dyma.masterprob()
         dyma.solve()
-        dyma.plotmap()
-        dyma.plotaltroute()
+        #dyma.plotmap()
+        #dyma.plotaltroute()
         print('time for generating routes: ', (end - start) * 10 ** 3, "ms")
         dyma.endmodel()
 
 
 if __name__ == "__main__":
     print('test')
+    #load_current_network_para()
+    #save_current_network_para()
     main(1)
     #vis = visualize()
     #vis.shownetwork()
